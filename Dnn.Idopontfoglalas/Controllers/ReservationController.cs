2eb94d459a6402de237017f0fc6bfa52d6ba1f3c
Rezsek Fanni <rezsekfz@gmail.com>
2025-04-26 20:00:52 +0200

    controller és view visszaállítása

---
+++ Dnn.Idopontfoglalas/Controllers/ReservationController.cs
@@ -0,0 +1,35 @@
+﻿using DotNetNuke.Web.Mvc.Framework.ActionFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using DotNetNuke.Web.Mvc.Framework.Controllers;
+using Dnn.Bce.Dnn.Idopontfoglalas.Services;
+
+namespace Dnn.Bce.Dnn.Idopontfoglalas.Controllers
+{
+    public class ReservationController : DnnController
+    {
+        private readonly IReservationService _reservationService;
+
+        public ReservationController()
+        {
+            _reservationService = new ReservationService();
+        }
+
+        public ActionResult Index()
+        {
+            bool isAdmin = User.IsInRole("Administrators");
+            int userId = User.UserID;
+
+            var reservations = _reservationService.GetReservations(isAdmin, userId);
+
+            return View(reservations);
+        }
+
+
+    }
+
+}
