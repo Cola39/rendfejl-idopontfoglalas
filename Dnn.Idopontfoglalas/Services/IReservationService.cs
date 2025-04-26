30a0877e6b5efbecdf63b62ef1e8b89fdda9569e
Rezsek Fanni <rezsekfz@gmail.com>
2025-04-26 11:47:29 +0200

    Services mappa, IReservationService.cs és ReservationService.cs létrehozva

---
+++ Dnn.Idopontfoglalas/Services/IReservationService.cs
@@ -0,0 +1,19 @@
+﻿using Dnn.Bce.Dnn.Idopontfoglalas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dnn.Bce.Dnn.Idopontfoglalas.Services
+{
+    public interface IReservationService
+    {
+        ReservationEntity GetReservationById(int id);
+        IEnumerable<ReservationEntity> GetAllReservations();
+        void CreateReservation(ReservationEntity reservation);
+        bool IsAvailable(int id);
+        void CancelReservation(int id);
+
+    }
+}
---
+++ Dnn.Idopontfoglalas/Services/ReservationService.cs
@@ -0,0 +1,60 @@
+﻿using Dnn.Bce.Dnn.Idopontfoglalas.Models;
+using DotNetNuke.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dnn.Bce.Dnn.Idopontfoglalas.Services
+{
+    public class ReservationService : IReservationService
+    {
+        public ReservationEntity GetReservationById(int id)
+        {
+            using (var context = DataContext.Instance())
+            {
+                var repo = context.GetRepository<ReservationEntity>();
+                return repo.GetById(id);
+            }
+        }
+
+        public IEnumerable<ReservationEntity> GetAllReservations()
+        {
+            using (var context = DataContext.Instance())
+            {
+                var repo = context.GetRepository<ReservationEntity>();
+                return repo.Get().ToList();
+            }
+        }
+
+        public void CreateReservation(ReservationEntity reservation)
+        {
+            using (var context = DataContext.Instance())
+            {
+                var repo = context.GetRepository<ReservationEntity>();
+                repo.Insert(reservation);
+            }
+        }
+
+        public bool IsAvailable(int id)
+        {
+            var reservation = GetReservationById(id);
+            return reservation != null && reservation.IsActive == true;
+        }
+
+        public void CancelReservation(int id)
+        {
+            var reservation = GetReservationById(id);
+            if (reservation != null)
+            {
+                reservation.IsActive = false;
+                using (var context = DataContext.Instance())
+                {
+                    var repo = context.GetRepository<ReservationEntity>();
+                    repo.Update(reservation);
+                }
+            }
+        }
+    }
+}
